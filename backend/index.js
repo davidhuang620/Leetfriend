9248399c439f5733240953af2b597d28a1cf8fbf
David <d12yes@gmail.com>
2019-06-02 10:07:45 -0500

    react render comments

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,66 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+const morgan = require("morgan")
+const pg = require("pg");
+const knex = require("knex");
+const cors = require("cors");
+
+// connect to postgre via knex
+const database = knex({
+    client: "pg",
+    connection: {
+        host: "127.0.0.1",
+        user: "David",
+        password: "",
+        database: "leetdb"
+    }
+});
+
 
 const app = express();
+
+// middleware
+app.use(morgan("short"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cors());
+
+// database.select("*").from("comment").then( data => {
+//     console.log(data);
+
+// // comment 
+
+app.post("/comments", (req, res) => {
+    const {Name, Comment} = req.body; 
+    database("comment")
+    .returning("*")
+    .insert({
+        username: Name,
+        comment: Comment,
+        createdtime: new Date()
+    })
+    .then( resp => {
+        res.json(resp);
+    })
+    .catch( err => {
+        res.send( err => { res.status(400).json("error")});
+    });
+})
+
+app.get("/comments", (req, res) => {
+    database.select("*").from("comment")
+    .then( data => {
+        res.json(data);
+    })
+})
+
+
 
 app.get("/", (req, res) => {
     console.log("get request");
     res.send("get request");
 })
 
-
-app.listen(3000, () => {
-    console.log("hello");
+app.listen(3002, () => {
+    console.log("hello, Server is listening 3002");
 })
---
+++ backend/play.js
@@ -0,0 +1,31 @@
+
+
+// const add = ((a, b, callback) => {
+
+//     setTimeout(() => {
+//         print(a, b)
+//     }, 2000)
+// })
+
+// const print = (a, b) => {
+//     console.log(a+b);
+// }
+
+// add(1, 4, print)
+
+// console.log(5)
+
+
+// const add = ((a, b) => {
+//     const print = setTimeout(() => {
+//         console.log(a + b)
+//     }, 2000)
+//     console.log(a + b);
+// })
+
+// add(1, 4)
+
+
+const print = setTimeout(() => {
+        console.log(a + b)
+}, 2000)
